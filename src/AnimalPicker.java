379570751b659553626180893a2d880eda7d1b15
b-regan11 <reganbill02@gmail.com>
2024-02-27 17:12:26 -0500

    Loops unless Cat/Dog

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class AnimalPicker {
     public static void main(String[] args) {
-        System.out.println("Choose an animal: [dog or cat]");
-
         Scanner scanner = new Scanner(System.in);
-        String chosenAnimal = scanner.nextLine().toLowerCase();
         
-        if (chosenAnimal.equals("dog")) {
-            System.out.println("DOG ASCII ART HERE");
-        } else if (chosenAnimal.equals("cat")) {
-            System.out.println("CAT ASCII ART HERE");
-        } else {
-            System.out.println("Invalid choice. Please choose either 'dog' or 'cat'");
-            System.exit(0);
+        while (true) {
+            System.out.println("Choose an animal: [dog or cat]");
+            String chosenAnimal = scanner.nextLine().toLowerCase();
+            
+            if (chosenAnimal.equals("dog")) {
+                System.out.println("DOG ASCII ART HERE");
+                break;
+            } else if (chosenAnimal.equals("cat")) {
+                System.out.println("CAT ASCII ART HERE");
+                break;
+            } else {
+                System.out.println("Invalid choice. Please choose either 'dog' or 'cat'");
+                //System.exit(0);
+            }
         }
-
         scanner.close();
     }
 }
